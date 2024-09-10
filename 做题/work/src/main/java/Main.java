c17ff2e5f0337bdbf265ec71c08c1968dac8017f
mirror <mirrorcloud3@gmail.com>
2024-09-10 20:42:57 +0800

    更新简历

--- 做题/work/src/main/java/Main.java
+++ 做题/work/src/main/java/Main.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 // 注意类名必须为 Main, 不要有任何 package xxx 信息
 public class Main {
@@ -10,29 +10,30 @@ public class Main {
             getResult(s);
         }
     }
-    private static void getResult(String s){
+
+    private static void getResult(String s) {
         StringBuffer stringBuffer = new StringBuffer();
-        
+
         int a = 0;
         int n = s.length();
         int ans = n / 8;
-        if("".equals(s)){
+        if ("".equals(s)) {
             System.out.println(s);
             return;
         }
 
-        if(n % 8 != 0){
-            a =8-  n % 8;
+        if (n % 8 != 0) {
+            a = 8 - n % 8;
             stringBuffer.append(s);
-            while(a !=0){
+            while (a != 0) {
                 stringBuffer.append("0");
                 a--;
             }
             ans++;
-            s= stringBuffer.toString();
+            s = stringBuffer.toString();
         }
-        for(int i = 0; i < ans; i++){
-            System.out.println(s.substring(0,8));
+        for (int i = 0; i < ans; i++) {
+            System.out.println(s.substring(0, 8));
             s = s.substring(8);
         }
     }
